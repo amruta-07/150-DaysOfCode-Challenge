febfb394e5f76b9cf224d9fdb77e6faf562818d4
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-06-04 13:15:01 +0530

    Add files via upload

--- Arrays/Day-3/RemoveDuplicates.java
+++ Arrays/Day-3/RemoveDuplicates.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public int removeDuplicates(int[] nums) {
+         int unique=0; //trace unique element in array
+     int current=0; //trace current element in array
+        while(current<nums.length-1){  //2<2  //uniq 1
+            
+            if(nums[current]!=nums[current+1]){ //compare with next element  1,2
+                nums[unique]=nums[current];
+                unique++;
+            }
+            current++;
+        }
+        
+        if(nums[current]!=nums[unique]){
+            nums[unique]=nums[current];
+        }
+        
+        return unique+1;
+        
+    }
+}
