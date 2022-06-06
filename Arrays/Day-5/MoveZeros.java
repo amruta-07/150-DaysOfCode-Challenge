2133c45b85ca3d98718508eaefc619843e625f58
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-06-07 00:37:29 +0530

    Add files via upload

--- Arrays/Day-5/MoveZeros.java
+++ Arrays/Day-5/MoveZeros.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int non_zero=0;
+        for(int start=0; start<nums.length; start++){
+            if(nums[start]!=0){
+                nums[non_zero]=nums[start];
+                non_zero++;
+               
+            }
+            
+        }
+        for(int end=non_zero; end<nums.length; end++){
+            nums[end]=0;
+        }
+        
+         
+    }
+}
