1ca431da03a3a5b12b54269c45dd4588f9aafa70
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-06-11 21:35:39 +0530

    Add files via upload

--- Arrays/Day-8/RotateImage.java
+++ Arrays/Day-8/RotateImage.java
@@ -1,1 +1,28 @@
-
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i=0; i<matrix.length; i++){
+            
+            for(int j=i; j<matrix.length; j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0; i<matrix.length; i++){
+            int start=0;
+            int end=matrix.length-1;
+            
+            while(start<end){
+                int temp=matrix[i][start];
+                matrix[i][start]=matrix[i][end];
+                matrix[i][end]=temp;
+                
+                start++;
+                end--;
+            }
+        
+        }
+        
+        
+    }
+}
