0901937e6fd86aa3a37e7958d63e39825039b29d
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-06-15 23:54:24 +0530

    Add files via upload

--- Linked List/Day-20/RemoveNthNode.java
+++ Linked List/Day-20/RemoveNthNode.java
@@ -1,1 +1,36 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp=head;
+        int size=0;
+        while(temp!=null){
+            temp=temp.next;
+            size++;
+        }
+        if(size==1 && n==1){
+            return null;
+        }
+        int total=size-n;
+        if(total<1){
+            head=head.next;
+            return head;
+        }
+        ListNode pre=head;
+        for(int i=0; i<total-1; i++){
+            pre=pre.next;
+        }
+        System.out.println(pre.val);
+        pre.next=pre.next.next;
+        return head;
+        
+    }
+}
