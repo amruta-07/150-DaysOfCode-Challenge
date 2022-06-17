f76cb4f52cb1dcb539f163cf9d916b429f8b31ef
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-06-17 09:47:27 +0530

    Add files via upload

--- Linked List/Day-21/MergeTwoSortedList.java
+++ Linked List/Day-21/MergeTwoSortedList.java
@@ -1,1 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
+        if(list1==null){
+            return list2;
+        }
+        if(list2==null){
+            return list1;
+        }
+        while(list1 !=null && list2 != null){
+            if(list1.val<=list2.val){
+                temp.next=list1;
+                temp=temp.next;
+                list1=list1.next;
+            }
+            else{
+                temp.next=list2;
+                list2=list2.next;
+                temp=temp.next;
 
+            }
+           
+            
+            
+        }
+        if(list1!=null){
+            temp.next=list1;
+        }
+        if(list2!=null){
+            temp.next=list2;
+        }
+        return dummy.next;
+        
+        
+    }
+}
