8a0e475cf4381e02850a9130affee2627e22552c
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-07-18 23:56:00 +0530

    Add files via upload

--- Linked List/day-22/Merge_K_sortedList.java
+++ Linked List/day-22/Merge_K_sortedList.java
@@ -1,1 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        List <Integer> list=new ArrayList<>();
+        for(int i=0; i<lists.length; i++){
+            ListNode head=lists[i];
+            while(head!=null){
+                list.add(head.val);
+                    head=head.next;
+            }
+            
+            
+        }
+        Collections.sort(list);
+        ListNode dummy=new ListNode(0); //converted integer to ListNode
+        ListNode temp=dummy;
+        for(int i=0; i<list.size(); i++){
+            temp.next=new ListNode(list.get(i));
+            temp=temp.next;
+        }
+        return dummy.next;
+        
+    }
+}
 
+//[1,4,5,1,3,4,2,6]
+// [1,1,2,3,4,4,5,6]
