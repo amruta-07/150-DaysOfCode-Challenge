38ad2ef0cd7b230530fffbe2f5459eadc7b627dd
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2023-01-07 20:21:42 +0530

    Add files via upload

---
+++ Strings/Remove_char.java
@@ -0,0 +1,12 @@
+
+// remove a given character from String
+
+public class Remove_char {
+    public static void main(String[] args) {
+        String str = "i am amruta";
+
+        System.out.println(str.replace("a", ""));
+
+    }
+
+}
